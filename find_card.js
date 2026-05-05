352ece72c3d24eb0d9f9735094d78078b2ead695
Your Name <vap98257@gmail.com>
2026-05-05 18:37:55 +0530

    chore: remove temp debug files

--- find_card.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:/Users/meetd/Downloads/City Real Space/properties.html', 'utf8');
-const si = html.indexOf("'<button class=\"card-fav\"");
-// Find Contact Now
-const ci = html.indexOf('Contact Now', si);
-console.log('Contact Now at:', ci);
-console.log(JSON.stringify(html.substring(ci - 50, ci + 200)));
